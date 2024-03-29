7f0595004005bc66323137427db9882d5c767fa7
Online Payments support team <96182451+online-payments-support-team@users.noreply.github.com>
2024-03-29 17:53:29 +0100

    Release 2.1.1.

--- OnlinePaymentsSDK/OPUtil.m
+++ OnlinePaymentsSDK/OPUtil.m
@@ -31,7 +31,7 @@
         NSString *deviceType = [self deviceType];
         self.metaInfo = @{
             @"platformIdentifier": platformIdentifier,
-            @"sdkIdentifier": @"objcClientSDK/v2.1.0",
+            @"sdkIdentifier": @"objcClientSDK/v2.1.1",
             @"sdkCreator": @"OnlinePayments",
             @"screenSize": screenSize,
             @"deviceBrand": @"Apple",
